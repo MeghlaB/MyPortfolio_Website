ecc324ef772cef18bc955359e16462ba4463894c
MeghlaB <meghlab964@gmail.com>
2025-01-04 23:38:25 +0600

    edit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -33,7 +33,7 @@ export default function Navbar() {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-hoverprimary rounded-box z-[1] mt-3 w-52 p-2 shadow">
                             {navLinks}
                         </ul>
                     </div>
