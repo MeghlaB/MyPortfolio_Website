0e1dca2c7fc95209b6b991db856d58306ed9d99d
MeghlaB <meghlab964@gmail.com>
2025-01-05 02:47:48 +0600

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,29 +12,29 @@ export default {
       keyframes: {
         'gradient-border': {
           '0%': {
-            border: '5px solid transparent',
+            border: '2px solid transparent',
             borderImage: 'linear-gradient(80deg, #1E1E36, #7731D8) 1',
             boxShadow: '0 0 15px rgba(119, 49, 216, 0.5)',
           },
           '25%': {
-            border: '5px solid transparent',
+            border: '2px solid transparent',
             borderImage: 'linear-gradient(80deg, #1F1F38, #7731D8) 1',
-            boxShadow: '0 0 25px rgba(119, 49, 216, 0.4)',
+         
           },
           '50%': {
-            border: '5px solid transparent',
+            border: '2px solid transparent',
             borderImage: 'linear-gradient(80deg, #7731D8, #1F1F38) 1',
-            boxShadow: '0 0 35px rgba(119, 49, 216, 0.3)',
+           
           },
           '75%': {
-            border: '5px solid transparent',
+            border: '2px solid transparent',
             borderImage: 'linear-gradient(80deg, #1F1F38, #1E1E36) 1',
-            boxShadow: '0 0 25px rgba(119, 49, 216, 0.4)',
+           
           },
           '100%': {
-            border: '5px solid transparent',
+            border: '2px solid transparent',
             borderImage: 'linear-gradient(80deg, #1E1E36, #7731D8) 1',
-            boxShadow: '0 0 15px rgba(119, 49, 216, 0.5)',
+      
           },
         },
       },
